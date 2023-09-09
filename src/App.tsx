70440ff18e3406b57f054b0fbab2df5e6b9a4939
gominzip <ahaminji@naver.com>
2023-09-09 21:20:47 +0900

    [INIT] test code 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { css } from '@emotion/react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          클릭하면 숫자가 올라가용 {count}
-        </button>
-        <h2>Lucy의 Arti 프로젝트 시작</h2>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	const style = css`
+		color: green;
+    font-size: 40px;
+    text-align: center;
+	`;
+	return (
+		<div className="App">
+			<div css={style}>arti-client</div>
+		</div>
+	);
 }
 
 export default App;
