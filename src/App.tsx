70928b3c1ac3aef01d4318ffabb9d5cc67ec187e
gominzip <ahaminji@naver.com>
2023-09-10 20:45:31 +0900

    [CHORE] 라우팅 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import { css } from '@emotion/react';
-import './styles/commonStyle.css'
-import NavBar from './components/common/NavBar';
-
 function App() {
-	const style = css`
-		color: #ff8c00;
-    font-size: 5rem;
-    text-align: center;
-	`;
 	return (
 		<div className="App">
-			<div css={style} className='headerLayout'>arti-client</div>
-			<NavBar/>
 		</div>
 	);
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/commonStyle.css';
+import NavBar from 'components/common/NavBar';
+
+const Home = () => {
+	return (
+		<>
+			<div className="headerLayout">arti-client</div>
+			<NavBar />
+		</>
+	);
+};
+
+export default Home;
