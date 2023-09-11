6b5f064777fdbc5679a18997a6fb0585affea25c
gominzip <ahaminji@naver.com>
2023-09-11 17:31:34 +0900

    [FEAT] 투표탭 배너 추가

--- src/components/ranking/RankingSection.tsx
+++ src/components/ranking/RankingSection.tsx
@@ -9,18 +9,36 @@ const RankingSection = () => {
 		justify-content: center;
 		align-items: center;
 	`;
-    const bannerSection = css`
-        width: 100%;
-        margin-top: 2rem;
-    `
-    const bannerImg = css`
-        width: 100%;
-    `
+	const bannerSection = css`
+		width: 100%;
+		aspect-ratio: 1 / 0.64;
+		margin-top: 2rem;
+		position: relative;
+		background-color: aliceblue;
+		overflow: -moz-hidden-unscrollable;
+	`;
+
+	const bannerChild = css`
+		position: absolute;
+	`;
+
+	const bannerImg = css`
+		width: 100%;
+	`;
+	const voteBtnImg = css`
+		width: 35%;
+		z-index: 1;
+		margin-top: 40px;
+		bottom: 2.8rem;
+		right: 2.8rem;
+		border-radius: 50%;
+		box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+	`;
 	return (
 		<div css={rankingSection}>
 			<div css={bannerSection}>
-				<img css={bannerImg} src="/img/voteBanner.png"/>
-                {/* <img src='img/goVoteBtn.png'/> */}
+				<img css={[bannerChild, bannerImg]} src="/img/voteBanner.png" />
+				<img css={[bannerChild, voteBtnImg]} src="img/goVoteBtn.png" />
 			</div>
 			<div>selectBtn</div>
 			<div>cardSection</div>
