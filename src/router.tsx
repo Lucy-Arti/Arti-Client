8a658aa4354d7ec28974849785b8db6107925797
CJY <cjy3458@naver.com>
2023-09-13 19:56:37 +0900

    [FEAT] 카카오 로그인 인가코드 저장

--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,31 @@
-import Home from "./pages/Home";
-import List from "./pages/List";
-import Mypage from "./pages/Mypage";
-import Voting from "./pages/Voting";
-import { createBrowserRouter } from "react-router-dom";
+import Home from './pages/Home';
+import List from './pages/List';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
+import Voting from './pages/Voting';
+import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/vote",
-        element: <Voting />,
-    },
-    {
-        path: "/productlist",
-        element: <List />,
-    },
-    {
-        path: "/mypage",
-        element: <Mypage />,
-    },
-])
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/vote',
+		element: <Voting />,
+	},
+	{
+		path: '/productlist',
+		element: <List />,
+	},
+	{
+		path: '/mypage',
+		element: <Mypage />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+]);
 
 export default router;
