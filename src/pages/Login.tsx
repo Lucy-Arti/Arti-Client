87877af8b57b82fe19a24caf3d6aee59d1045532
gominzip <ahaminji@naver.com>
2023-09-21 19:51:50 +0900

    [FIX] 카카오 로그인 리다이렉트 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { kakaoURL } from '@/components/login/loginInfo';
 import { css } from '@emotion/react';
 import '@styles/commonStyle.css';
@@ -27,9 +26,6 @@ const Login = ({ content }: { content: string }) => {
 	`;
 
 
-	const Rest_API_key = '209c9251e18aa84300b9f4dc8047c6cd';
-	const Redirect_uri = 'http://localhost:3000/login';
-	const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_API_key}&redirect_uri=${Redirect_uri}&response_type=code`;
 
 	const handleLogin = () => {
 		window.location.href = kakaoURL;
