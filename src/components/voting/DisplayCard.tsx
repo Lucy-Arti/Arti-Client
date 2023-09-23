3a57bb13fe3f9d45320393bc32c2c84ca547dcfb
gominzip <ahaminji@naver.com>
2023-09-23 22:48:45 +0900

    [FEAT] 투표이미지 확대 구현

--- src/components/voting/DisplayCard.tsx
+++ src/components/voting/DisplayCard.tsx
@@ -1,7 +1,15 @@
 import { WorldcupSample } from '@/types/request';
 import { css } from '@emotion/react';
+import { useState } from 'react';
+import ZoomImg from './ZoomImg';
 
-const DisplayCard = ({ data}: { data: WorldcupSample; }) => {
+const DisplayCard = ({ data }: { data: WorldcupSample }) => {
+	const [isZoomed, setIsZoomed] = useState(false);
+
+	const handleZoom = () => {
+		setIsZoomed((prevIsZoomed) => !prevIsZoomed);
+	};
+
 	const card = css`
 		width: 45%;
 		height: 350px;
@@ -9,36 +17,37 @@ const DisplayCard = ({ data}: { data: WorldcupSample; }) => {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-        gap: 1rem;
+		gap: 1rem;
 	`;
 	const contentSection = css`
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 95%;
+		width: 100%;
 		height: 250px;
 		object-fit: cover;
 		overflow: hidden;
-        border-radius: 5px;
-        &:hover{
-            border: 3px solid #6BDA01;
-            background-color: #6BDA01;
-        }
-        @media (min-width:0px){
-            height: 230px;
-        }
-        @media (min-width:576px){
-            height: 250px;
-        }
+		border-radius: 5px;
+		&:hover {
+			border: 3px solid #6bda01;
+			background-color: #6bda01;
+		}
+		@media (min-width: 0px) {
+			height: 230px;
+		}
+		@media (min-width: 470px) {
+			height: 250px;
+			width: 95%;
+		}
 	`;
 	const productimg = css`
 		border-radius: 5px;
-        @media (min-width:0px){
-            height: 100%;
-        }
-        @media (min-width:576px){
-            width: 100%;
-        }
+		@media (min-width: 0px) {
+			height: 100%;
+		}
+		@media (min-width: 470px) {
+			width: 100%;
+		}
 	`;
 	const info = css`
 		width: 95%;
@@ -53,8 +62,15 @@ const DisplayCard = ({ data}: { data: WorldcupSample; }) => {
 			</div>
 			<div css={info}>
 				<div>{data.product}</div>
-				<img src="/img/voteSearch.svg" css={css`cursor: pointer;`}/>
+				<img
+					src="/img/voteSearch.svg"
+					css={css`
+						cursor: pointer;
+					`}
+					onClick={handleZoom}
+				/>
 			</div>
+			{isZoomed ? <ZoomImg image={data.src} setIsZoomed={setIsZoomed} /> : ''}
 		</div>
 	);
 };
