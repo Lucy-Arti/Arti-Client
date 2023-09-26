a97188403e16f1fd6cb84f61b8d85978200e737e
gominzip <ahaminji@naver.com>
2023-09-26 21:42:48 +0900

    [FEAT] helmet으로 페이지별 메타태그 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,10 @@ import { css } from '@emotion/react';
 import LandingSection from '@/components/landing/LandingSection';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
+	const link = location.href;
 	const flexColumn = css`
 		display: flex;
 		flex-direction: column;
@@ -14,6 +16,12 @@ const Home = () => {
 
 	return (
 		<div css={flexColumn}>
+			<Helmet>
+				<title>Arti</title>
+				<meta name="description" content="투표 기반 신진 패션 디자이너 플랫폼" />
+				<meta property="og:image" content="/img/desktopBanner1.png" />
+				<meta property="og:url" content={link} />
+			</Helmet>
 			<Header where='main'/>
 			<NavBar />
 			<LandingSection />
