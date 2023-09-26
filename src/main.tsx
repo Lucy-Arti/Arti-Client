c01061929d86dd4c77433d5ed6cbcd2c28776132
gominzip <ahaminji@naver.com>
2023-09-26 22:05:14 +0900

    [FIX] import문 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@styles/global.css';
 import { RecoilRoot } from 'recoil';
-import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<RecoilRoot>
-		<HelmetProvider>
-			<App />
-		</HelmetProvider>
+		<App />
 	</RecoilRoot>,
 );
