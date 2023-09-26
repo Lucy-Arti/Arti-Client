69862a386c4927634e19fdaab29f255ca8b29974
Gyeongbin <tarabin49@gmail.com>
2023-09-26 22:51:52 +0900

    [FEAT]마이페이지 컴포넌트 구현

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,14 +1,46 @@
 import '@styles/commonStyle.css';
 import NavBar from '@common/NavBar';
+import { css } from '@emotion/react';
+import MypageUpperContent from '@/components/mypage/MypageUpperContent';
+import MypageExternalContent from '@/components/mypage/MypageExternalContent';
 
 const Mypage = () => {
+	const spaceBetween = css`
+		justify-content: space-between;
+	`
+	const flexColumn = css`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	`
+	const ArticleWrapper = css`
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+		align-items: center;
+	`
+	const gapDesign = css`
+		background-color: #f5f5f5;
+		height: 1rem;
+		width: 100%;
+		margin-top: 2rem;
+	`
 	return (
 		<>
+			<div css={flexColumn}>
 			<div className="headerLayout">
-				<div className="headerComponent">dd</div>
+				<div css={spaceBetween} className='headerComponent'>
+					<img src='/img/blackLogo.svg'/>
+					<img src='/img/search.svg'/>
+				</div>
+			</div>
 			</div>
 			<NavBar />
-			<div>Mypage</div>
+			<div css={ArticleWrapper}>
+				<MypageUpperContent />
+				<div css={gapDesign} />
+				<MypageExternalContent />
+			</div>
 		</>
 	);
 };
