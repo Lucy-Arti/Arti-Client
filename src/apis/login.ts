2d4106c51e7cf9e4ba86520c06df5a91c01a8db6
CJY <cjy3458@naver.com>
2023-09-27 18:53:12 +0900

    [FEAT] 만료기한에 따른 API 코드 생성

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -11,7 +11,7 @@ export const postCode = async () => {
 	try {
 		const response = await axios.post(`${baseURL}/api/v1/oauth2/kakao/callback`, body);
 		console.log('인가코드 전송 및 전용 토큰 응답');
-		const accessToken = response.data.accessToekn;
+		const accessToken = response.data.accessToken;
 		const refreshToken = response.data.refreshToken;
 
 		localStorage.setItem('access', accessToken);
@@ -22,4 +22,46 @@ export const postCode = async () => {
 		console.error('Error sending POST request:', error);
 		throw error;
 	}
+};
+
+export const getNewRefreshToken = async () => {
+	const accessToken = localStorage.getItem('access');
+	const refreshToken = localStorage.getItem('refresh');
+
+	const response = await axios.post(
+		`${baseURL}/newRefresh`,
+		{
+			refreshToken,
+		},
+		{
+			headers: {
+				Authorization: accessToken,
+			},
+		},
+	);
+
+	return response.data;
+};
+
+export const getAuthAxios = (token: string) => {
+	const authAxios = axios.create({
+		baseURL: baseURL,
+		headers: {
+			Authorization: token,
+		},
+	});
+
+	authAxios.interceptors.response.use(
+		(res) => res,
+		async (error) => {
+			if (error.reponse.status === 401) {
+				const { accessToken, refreshToken } = await getNewRefreshToken();
+				error.config.headers.Authorization = accessToken;
+				localStorage.setItem('access', accessToken);
+				localStorage.setItem('refresh', refreshToken);
+				return (await axios.get(error.config.url, error.config)).data;
+			}
+		},
+	);
+	return authAxios;
 };
