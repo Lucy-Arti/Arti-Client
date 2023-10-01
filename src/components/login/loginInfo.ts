d116cc22e78762d39d7e81f87c59f5e045701667
CJY <cjy3458@naver.com>
2023-10-01 17:18:32 +0900

    [FEAT] 로그인 배포환경 실험

--- src/components/login/loginInfo.ts
+++ src/components/login/loginInfo.ts
@@ -1,4 +1,3 @@
 export const Rest_API_key = '209c9251e18aa84300b9f4dc8047c6cd';
-export const Redirect_uri = 'http://localhost:3000/kakaologin';
-export const Redirect_uri2 = 'http://localhost:3000/mypage';
+export const Redirect_uri = 'https://lucy-arti.netlify.app/kakaologin';
 export const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_API_key}&redirect_uri=${Redirect_uri}&response_type=code`;
