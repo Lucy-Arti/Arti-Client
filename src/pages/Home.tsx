a01c7ec4058a8303c850d08dd4120e9feb143a90
CJY <cjy3458@naver.com>
2023-10-02 22:14:55 +0900

    [FEAT] 로그인 구현 완료 + recoil-persist 설치

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,36 @@ import { css } from '@emotion/react';
 import LandingSection from '@/components/landing/LandingSection';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
+import { useEffect } from 'react';
+import { getUserInfo } from '@/apis/login';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { isLoginAtom, userEmailAtom, userNameAtom } from '@/utils/state';
 
 const Home = () => {
+	const isLogin = useRecoilValue(isLoginAtom);
+	const setUserEmail = useSetRecoilState(userEmailAtom);
+	const setUserName = useSetRecoilState(userNameAtom);
+
 	const flexColumn = css`
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	`;
 
+	useEffect(() => {
+		if (localStorage.getItem('access')) {
+			const getUserProfile = async () => {
+				const data = await getUserInfo();
+				setUserEmail(data.email);
+				setUserName(data.userName);
+			};
+			getUserProfile();
+		}
+	});
+
 	return (
 		<div css={flexColumn}>
-			<Header where='main'/>
+			<Header where="main" />
 			<NavBar />
 			<LandingSection />
 			<Footer />
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -4,23 +4,34 @@ import Header from '@/components/common/Header';
 import MypageUpperContent from '@/components/mypage/MypageUpperContent';
 import MypageExternalContent from '@/components/mypage/MypageExternalContent';
 import { css } from '@emotion/react';
+import { isLoginAtom, userEmailAtom, userNameAtom } from '@/utils/state';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 const Mypage = () => {
+	const isLogin = useRecoilValue(isLoginAtom);
+	const [email, setEmail] = useRecoilState(userEmailAtom);
+	const [name, setName] = useRecoilState(userNameAtom);
+
 	const ArticleWrapper = css`
 		display: flex;
 		width: 100%;
 		flex-direction: column;
 		align-items: center;
-	`
+	`;
 	const gapDesign = css`
 		background-color: #f5f5f5;
 		height: 1rem;
 		width: 100%;
 		margin-top: 2rem;
-	`
+	`;
+
+	console.log(isLogin);
+	console.log(email);
+	console.log(name);
+
 	return (
 		<>
-			<Header where='main'/>
+			<Header where="main" />
 			<NavBar />
 			<div css={ArticleWrapper}>
 				<MypageUpperContent />
