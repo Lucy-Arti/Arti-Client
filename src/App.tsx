87bd6b8ce8245ecd4caca5fc400b95c9b27d7b61
CJY <cjy3458@naver.com>
2023-10-03 16:44:27 +0900

    [FEAT] 채널톡 연동 성공

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { RecoilRoot } from 'recoil';
+import ChannelService from './ChannelService';
 
 function App() {
-	// ChannelService.loadScript();
+	ChannelService.loadScript();
+
+	ChannelService.boot({
+		pluginKey: 'd6edfe98-afd0-4989-be6a-3b13b2aa2c71', // fill your plugin key
+	});
+
 	return (
 		<RecoilRoot>
 			<RouterProvider router={router} />
