5cfca96a733c9f3ff7be3c4e11affd0f74912a9e
CJY <cjy3458@naver.com>
2023-10-03 18:55:18 +0900

    [FEAT] GTM 설치

---
+++ src/GoogleTagManager.tsx
@@ -0,0 +1,9 @@
+// import { useEffect } from 'react';
+// import TagManager from 'react-gtm-module';
+
+// const GoogleTagManager = (gtmId: String) => {
+// 	useEffect(() => {});
+// 	return <div>GoogleTagManager</div>;
+// };
+
+// export default GoogleTagManager;
