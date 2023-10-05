d0d623798f50527bd1866d4d0187583744fe9eea
Gyeongbin <tarabin49@gmail.com>
2023-10-05 21:58:02 +0900

    [FEAT]디자이너 페이지 api 연결 및 설정 완료

---
+++ src/apis/designer.ts
@@ -0,0 +1,24 @@
+import axios from "axios"
+const baseURL = `https://arti-fashion.shop/`;
+
+export const GetDesignerProduct = async(idx:string) => {
+    try{
+        const response = await axios.get(`${baseURL}api/v1/clothes/designer/${idx}`);
+        console.log(response.data);
+        return response;
+    } catch(error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export const GetDesignerDetail = async(id:string) => {
+    try{
+        const response = await axios.get(`${baseURL}api/v1/designers/${id}`);
+        console.log(response.data);
+        return response;
+    } catch(error) {
+        console.log(error);
+        return false;
+    }
+}
