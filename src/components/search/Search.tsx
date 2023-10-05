fcb4dee338aa64068ebadea18141f3b88fad2bec
Gyeongbin <tarabin49@gmail.com>
2023-10-05 20:08:57 +0900

    [FEAT]검색 페이지 업데이트

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,40 @@
 import { css } from '@emotion/react';
 import { useRef, useState } from 'react';
 import { FiChevronLeft } from 'react-icons/fi';
+import { ProductType } from '../list/ListView';
+import { GetSearchProductList } from '@/apis/search';
+import ListCard from '../list/ListCard';
+import ModalLogin from '../list/ModalLogin';
+import ModalProductSaved from '../list/ModalProductSaved';
+import ModalProductUnsaved from '../list/ModalProductUnsaved';
 
 const Search = () => {
+    const inputText = useRef<HTMLInputElement>(null);
+    const productList : ProductType[] = [];
+	const [savedModalIsOpen, setSavedModalIsOpen] = useState(false);
+	const [unsavedModalIsOpen, setUnsavedModalIsOpen] = useState(false);
+	const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
+	const [products, setProducts] = useState<ProductType[]>(productList);
+    const getProducts = async() => {
+        if (inputText.current === null || inputText.current.value === ''){
+            const result = await GetSearchProductList('');
+            if(result===false) {
+                alert("불러오기 오류 발생");
+            } else {
+                setProducts(productList.concat(result.data));
+            }
+        } else {
+            const result = await GetSearchProductList(inputText.current.value);
+            if(result===false) {
+                alert("불러오기 오류 발생");
+            } else {
+                setProducts(productList.concat(result.data));
+            }
+        }
+	}
+    const handleClick = () => {
+        getProducts();
+    }
     const searchBox = css`
         display: flex;
         width: 80%;
@@ -10,11 +42,20 @@ const Search = () => {
         background-color: rgba(245, 245, 245, 1);
         border-radius: 10px;
     `
-    const inputText = useRef<HTMLInputElement>(null);
-    const [isClicked, setIsClicked] = useState(false);
-    const handleClick = () => {
-        setIsClicked(true);
-    }
+    const gridWrapper = css`
+		display: grid;
+		height: 80vh;
+		margin: 2rem;
+		/* border: 1px solid black; */
+		grid-template-columns: repeat(2, 1fr);
+  		grid-template-rows: repeat(3,1fr);
+  		grid-auto-flow: row;
+		grid-gap: 2rem;
+		overflow-y: scroll;
+		&::-webkit-scrollbar {
+			display: none;
+		}
+	`
   return (
     <>
     <div className="headerLayout" css={css`
@@ -32,14 +73,38 @@ const Search = () => {
             <img src="/img/search.svg" onClick={handleClick} />
         </div>
     </div>
-    <div>
         {
-            (isClicked === true) && (inputText.current?.value !== '') && 
-            <div>
-                {inputText.current?.value} 에 대한 검색결과
-            </div>
-            
-        }
+			(loginModalIsOpen === true) && 
+			<ModalLogin setLoginModalIsOpen={setLoginModalIsOpen} />
+		}
+		{
+			(savedModalIsOpen === true) && <ModalProductSaved />
+		}
+		{
+			(unsavedModalIsOpen === true) && <ModalProductUnsaved />
+		}
+    <div>
+        <div css={gridWrapper}>
+            {products && 
+            products.map((product:ProductType, idx:number) => (
+				<ListCard 
+					key={idx}
+					clothesId={product.clothesId}
+					createdAt={product.createdAt}
+					updatedAt={product.updatedAt}
+					detailImg={product.detailImg}
+					likeCount={product.likeCount}
+					clothesName={product.clothesName}
+					preview={product.preview}
+					designerId={product.designerId}
+					designerName={product.designerName}
+					score={product.score}
+					setSavedModalIsOpen={setSavedModalIsOpen} 
+					setUnsavedModalIsOpen={setUnsavedModalIsOpen} 
+					setLoginModalIsOpen={setLoginModalIsOpen} 
+				/>
+			))}
+        </div>
     </div>
     </>
   )
