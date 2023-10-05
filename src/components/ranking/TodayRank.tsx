833593e842ba2058018dad44ce80543df621eb70
gominzip <ahaminji@naver.com>
2023-10-06 02:32:33 +0900

    [FEAT] 전체랭킹 백 연결

--- src/components/ranking/TodayRank.tsx
+++ src/components/ranking/TodayRank.tsx
@@ -1,31 +1,29 @@
-import { css } from "@emotion/react"
-import RankingDataList from "./RankingDataList";
-import { useEffect, useState } from "react";
-import { getTotalScoreList } from "@/apis/vote";
+import { css } from '@emotion/react';
+import RankingDataList from './RankingDataList';
+import { useEffect, useState } from 'react';
+import { getTotalScoreList } from '@/apis/vote';
 
 const TodayRank = () => {
-	const [rankData,setRankData]=useState([]);
+	const [rankData, setRankData] = useState([]);
 
 	useEffect(() => {
-		if (localStorage.getItem('access')) {
-			const getTotalScoreData = async () => {
-				const result = await getTotalScoreList();
-				setRankData(result);
-			};
-			getTotalScoreData();
-		}
+		const getTotalScoreData = async () => {
+			const result = await getTotalScoreList();
+			setRankData(result);
+		};
+		getTotalScoreData();
 	});
 
 	const wrapper = css`
 		width: 100%;
 		margin-top: 3.12rem;
-    height: fit-content;
+		height: fit-content;
 	`;
 	return (
 		<div css={wrapper}>
 			<RankingDataList datas={rankData} />
 		</div>
 	);
-}
+};
 
-export default TodayRank
+export default TodayRank;
