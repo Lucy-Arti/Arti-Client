a2fd340904b43488040ef8483e846c3789a52f9e
gominzip <ahaminji@naver.com>
2023-10-06 01:36:25 +0900

    [FIX] 모바일 랜딩 에러 수정

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,11 +1,9 @@
-import LandingSection2 from '@/components/landing2/LandingSection2';
 import '@styles/commonStyle.css';
 import { Outlet } from 'react-router-dom';
 
 const List = () => {
 	return (
 		<>
-		<LandingSection2/>
 			<Outlet />
 		</>
 	);
