7bf812a9f78a833b5a52fa735cae4aabcfd3c481
gominzip <ahaminji@naver.com>
2023-10-06 04:47:47 +0900

    [CHORE] 검색창 수정

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -3,126 +3,139 @@ import { useRef, useState } from 'react';
 import { FiChevronLeft } from 'react-icons/fi';
 import { ProductType } from '../list/ListView';
 import { GetSearchProductList } from '@/apis/search';
-import ListCard from '../list/ListCard';
 import ModalLogin from '../list/ModalLogin';
 import ModalProductSaved from '../list/ModalProductSaved';
 import ModalProductUnsaved from '../list/ModalProductUnsaved';
 import SearchCard from './SearchCard';
 import { GetAllProductLists } from '@/apis/list';
-import Footer from '../common/Footer';
 
 const Search = () => {
-    const inputText = useRef<HTMLInputElement>(null);
-    const productList : ProductType[] = [];
+	const inputText = useRef<HTMLInputElement>(null);
+	const productList: ProductType[] = [];
 	const [savedModalIsOpen, setSavedModalIsOpen] = useState(false);
 	const [unsavedModalIsOpen, setUnsavedModalIsOpen] = useState(false);
 	const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
 	const [products, setProducts] = useState<ProductType[]>(productList);
-    const getProducts = async() => {
-        if (inputText.current === null || inputText.current.value === ''){
-            const result = await GetAllProductLists();
-            if(result===false) {
-                alert("불러오기 오류 발생");
-            } else {
-                var newArr:ProductType[] = [];
-                var preData:ProductType[] = result.data;
-                var arraySize = preData.length;
-                for (let i = 0; i < arraySize; i++) {
-                    let randomNum = Math.floor( Math.random() * (preData.length - i) );
-                    let element = preData.splice(randomNum, 1);
-                    newArr = newArr.concat(element);
-                }
-                // setProducts(products.concat(result.data));
-                setProducts(productList.concat(newArr));
-            }
-        } else {
-            const result = await GetSearchProductList(inputText.current.value);
-            if(result===false) {
-                alert("불러오기 오류 발생");
-            } else {
-                setProducts(productList.concat(result.data));
-            }
-        }
-	}
-    const handleClick = () => {
-        getProducts();
-    }
-    const searchBox = css`
-        display: flex;
-        width: 80%;
-        height: 3rem;
-        background-color: rgba(245, 245, 245, 1);
-        border-radius: 10px;
-    `
-    const gridWrapper = css`
+	const getProducts = async () => {
+		if (inputText.current === null || inputText.current.value === '') {
+			const result = await GetAllProductLists();
+			if (result === false) {
+				alert('불러오기 오류 발생');
+			} else {
+				var newArr: ProductType[] = [];
+				var preData: ProductType[] = result.data;
+				var arraySize = preData.length;
+				for (let i = 0; i < arraySize; i++) {
+					let randomNum = Math.floor(Math.random() * (preData.length - i));
+					let element = preData.splice(randomNum, 1);
+					newArr = newArr.concat(element);
+				}
+				// setProducts(products.concat(result.data));
+				setProducts(productList.concat(newArr));
+			}
+		} else {
+			const result = await GetSearchProductList(inputText.current.value);
+			if (result === false) {
+				alert('불러오기 오류 발생');
+			} else {
+				setProducts(productList.concat(result.data));
+			}
+		}
+	};
+	const handleClick = () => {
+		getProducts();
+	};
+	const fixed = css`
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 70px;
+		top: 0%;
+		background-color: white;
+        margin-top: 10px;
+	`;
+	const searchBox = css`
+		display: flex;
+		width: 80%;
+		height: 40px;
+		background-color: rgba(245, 245, 245, 1);
+		border-radius: 10px;
+	`;
+	const margin = css`
+		height: 180px;
+	`;
+	const gridWrapper = css`
 		display: grid;
 		/* height: 80vh; */
 		margin: 2rem;
 		/* border: 1px solid black; */
 		grid-template-columns: repeat(2, 1fr);
-  		/* grid-template-rows: repeat(3,1fr); */
-  		grid-auto-flow: row;
+		/* grid-template-rows: repeat(3,1fr); */
+		grid-auto-flow: row;
 		grid-gap: 2rem;
 		overflow-y: scroll;
 		&::-webkit-scrollbar {
 			display: none;
 		}
-	`
-  return (
-    <>
-    <div className="headerLayout" css={css`
-        gap: 2rem;
-    `}>
-        <FiChevronLeft cursor="pointer" size="26px" onClick={() => history.back()} />
-        <div css={searchBox}>
-            <input css={css`
-                width: 87%;
-                background-color: transparent;
-                border: none;
-                outline: none;
-                margin-left: 1rem;
-            `} ref={inputText} />
-            <img css={css`
-                width: 25px;
-                :hover{cursor: pointer;}
-            `} src="/img/search.svg" onClick={handleClick} />
-        </div>
-    </div>
-        {
-			(loginModalIsOpen === true) && 
-			<ModalLogin setLoginModalIsOpen={setLoginModalIsOpen} />
-		}
-		{
-			(savedModalIsOpen === true) && <ModalProductSaved />
-		}
-		{
-			(unsavedModalIsOpen === true) && <ModalProductUnsaved />
-		}
-    <div>
-        <div css={gridWrapper}>
-            {products && 
-            products.map((product:ProductType, idx:number) => (
-				<SearchCard 
-					key={idx}
-					clothesId={product.clothesId}
-					createdAt={product.createdAt}
-					updatedAt={product.updatedAt}
-					detailImg={product.detailImg}
-					likeCount={product.likeCount}
-					clothesName={product.clothesName}
-					preview={product.preview}
-					designerId={product.designerId}
-					designerName={product.designerName}
-					score={product.score}
-					setSavedModalIsOpen={setSavedModalIsOpen} 
-					setUnsavedModalIsOpen={setUnsavedModalIsOpen} 
-					setLoginModalIsOpen={setLoginModalIsOpen} 
-				/>
-			))}
-        </div>
-    </div>
-    </>
-  )
-}
+	`;
+	return (
+		<div>
+			<div css={fixed}>
+				<FiChevronLeft cursor="pointer" size="26px" onClick={() => history.back()} />
+				<div css={searchBox}>
+					<input
+						css={css`
+							width: 87%;
+							background-color: transparent;
+							border: none;
+							outline: none;
+							margin-left: 1rem;
+                            font-size: 2rem;
+						`}
+						ref={inputText}
+					/>
+					<img
+						css={css`
+							width: 25px;
+							:hover {
+								cursor: pointer;
+							}
+						`}
+						src="/img/search.svg"
+						onClick={handleClick}
+					/>
+				</div>
+			</div>
 
-export default Search
+			{loginModalIsOpen === true && <ModalLogin setLoginModalIsOpen={setLoginModalIsOpen} />}
+			{savedModalIsOpen === true && <ModalProductSaved />}
+			{unsavedModalIsOpen === true && <ModalProductUnsaved />}
+			<div css={margin}>
+				<div css={gridWrapper}>
+					{products &&
+						products.map((product: ProductType, idx: number) => (
+							<SearchCard
+								key={idx}
+								clothesId={product.clothesId}
+								createdAt={product.createdAt}
+								updatedAt={product.updatedAt}
+								detailImg={product.detailImg}
+								likeCount={product.likeCount}
+								clothesName={product.clothesName}
+								preview={product.preview}
+								designerId={product.designerId}
+								designerName={product.designerName}
+								score={product.score}
+								setSavedModalIsOpen={setSavedModalIsOpen}
+								setUnsavedModalIsOpen={setUnsavedModalIsOpen}
+								setLoginModalIsOpen={setLoginModalIsOpen}
+							/>
+						))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
