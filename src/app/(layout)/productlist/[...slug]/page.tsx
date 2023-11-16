86cdb41a5470f361a455cfd67e2821f980d55bcb
gominzip <ahaminji@naver.com>
2023-11-17 01:25:51 +0900

    chore : productlist ㅣ이동

---
+++ src/app/(layout)/productlist/[...slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@styles/commonStyle.css';
+import ProductDetail from '@/components/productlist/ProductDetail';
+
+const page = () => {
+	return (
+		<>
+			<ProductDetail />
+		</>
+	);
+};
+
+export default page;
---
+++ src/app/(layout)/productlist/page.tsx
@@ -0,0 +1,13 @@
+import ListView from '@/components/productlist/ListView'
+import React from 'react'
+import '@styles/commonStyle.css';
+
+const page = () => {
+  return (
+    <>
+      <ListView />
+    </>
+  )
+}
+
+export default page
