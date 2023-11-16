75b1b1b42bc14ab0a206b02f0b4ae106309357f9
gominzip <ahaminji@naver.com>
2023-11-17 01:33:21 +0900

    chore : vote 폴더 이동

---
+++ src/app/(layout)/vote/page.tsx
@@ -0,0 +1,12 @@
+import Voting from '@/components/voting/Voting';
+import React from 'react';
+
+const page = () => {
+	return (
+		<>
+			<Voting />
+		</>
+	);
+};
+
+export default page;
---
+++ src/app/(layout)/vote/userPick/page.tsx
@@ -0,0 +1,12 @@
+import FinalPick from '@/components/voting/FinalPick';
+import React from 'react';
+
+const page = () => {
+	return (
+		<>
+			<FinalPick />
+		</>
+	);
+};
+
+export default page;
