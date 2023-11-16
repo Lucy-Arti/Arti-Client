fa0746465d3ff24abc9338f23472777885a77bd9
gominzip <ahaminji@naver.com>
2023-11-17 01:27:50 +0900

    chore : mypage 이동

---
+++ src/app/(layout)/mypage/page.tsx
@@ -0,0 +1,12 @@
+import MypageBasicView from '@/components/mypage/MypageBasicView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <MypageBasicView />
+    </>
+  )
+}
+
+export default page
