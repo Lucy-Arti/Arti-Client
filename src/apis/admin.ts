dcdc510c66c06535d77cbb2bc024367c3129bfee
gominzip <ahaminji@naver.com>
2023-11-24 16:50:52 +0900

    feat : #36 - 어드민 전체 옷 리스트 조회 추가

---
+++ src/apis/admin.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const baseURL = `https://arti-fashion.shop/`;
+
--- src/components/admin/tab/ClothesTab.tsx
+++ src/components/admin/tab/ClothesTab.tsx
@@ -1,21 +1,40 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Column } from 'react-table';
 import styled from 'styled-components';
 import { CLOTHES_INFO_COLUMNS } from '../common/columns';
 import { ClothesInfoData } from '@/types/request';
 import { clothesMockData } from '../common/MOCK_DATA';
 import PaginationTable from '../common/PaginationTable';
+import { GetAllProductLists } from '@/apis/list';
 
 const ClothesTab = () => {
 	const columns: Column[] = useMemo(() => CLOTHES_INFO_COLUMNS, []);
-	const data: ClothesInfoData[] = useMemo(() => clothesMockData, []);
+	const [data, setData] = useState<ClothesInfoData[]>();
+	// const data: ClothesInfoData[] = useMemo(() => clothesMockData, []);
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await GetAllProductLists();
+				if (response && response.data) {
+					setData(response.data);
+				} else {
+					console.log('Failed to fetch data');
+				}
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
+		getData();
+	}, []);
+
 	return (
 		<Section>
 			<Top>
 				<div className="title">옷</div>
 				<Btn>+등록하기</Btn>
 			</Top>
-			<PaginationTable columns={columns} data={data} />
+			{data ? <PaginationTable columns={columns} data={data} /> : <></>}
 		</Section>
 	);
 };
@@ -53,10 +72,10 @@ const Btn = styled.button`
 	justify-content: center;
 	align-items: center;
 	gap: 1.25rem;
-  color: var(--white, #FFF);
-font-family: Pretendard;
-font-size: 2.5rem;
-font-style: normal;
-font-weight: 600;
-background: #000;
+	color: var(--white, #fff);
+	font-family: Pretendard;
+	font-size: 2.5rem;
+	font-style: normal;
+	font-weight: 600;
+	background: #000;
 `;
