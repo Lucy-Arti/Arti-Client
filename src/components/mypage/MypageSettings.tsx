60d04bdf54f8df1832a589f7cbb604b9f6491005
CJY <cjy3458@naver.com>
2023-11-25 04:11:37 +0900

    [FEAT] 로그아웃 구현

--- src/components/mypage/MypageSettings.tsx
+++ src/components/mypage/MypageSettings.tsx
@@ -1,28 +1,45 @@
-'use client'
-import React from 'react'
-import Header from '../common/Header'
-import styled from 'styled-components'
+'use client';
+import React from 'react';
+import Header from '../common/Header';
+import styled from 'styled-components';
 import { FiChevronRight } from 'react-icons/fi';
+import { useSetRecoilState } from 'recoil';
+import { isLoginAtom, userEmailAtom, userNameAtom, userPhotoAtom, userRoleAtom } from '@/app/recoilContextProvider';
+import { useRouter } from 'next/navigation';
 
 const MypageSettings = () => {
-  return (
-    <>
-        <Header where="내 정보 수정" />
-        <ArticleWrapper>
-            <ListBox className='with-bottom'>
-                <div>프로필 수정</div>
-                <FiChevronRight size="26px" />
-            </ListBox>
-            <ListBox>
-                <div>로그아웃</div>
-                <FiChevronRight size="26px" />
-            </ListBox>
-        </ArticleWrapper>
-    </>
-  )
-}
+	const setUserEmail = useSetRecoilState(userEmailAtom);
+	const setRole = useSetRecoilState(userRoleAtom);
+	const setUserName = useSetRecoilState(userNameAtom);
+	const setLogin = useSetRecoilState(isLoginAtom);
+	const router = useRouter();
 
-export default MypageSettings
+	const logout = () => {
+		setUserEmail('');
+		setRole('');
+		setUserEmail('');
+		setUserName('');
+		setLogin(false);
+		router.push('/');
+	};
+	return (
+		<>
+			<Header where="내 정보 수정" />
+			<ArticleWrapper>
+				<ListBox className="with-bottom">
+					<div>프로필 수정</div>
+					<FiChevronRight size="26px" />
+				</ListBox>
+				<ListBox onClick={logout}>
+					<div>로그아웃</div>
+					<FiChevronRight size="26px" />
+				</ListBox>
+			</ArticleWrapper>
+		</>
+	);
+};
+
+export default MypageSettings;
 
 const ArticleWrapper = styled.div`
 	display: flex;
@@ -32,14 +49,14 @@ const ArticleWrapper = styled.div`
 `;
 
 const ListBox = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 90%;
-    justify-content: space-between;
-    padding: 1rem 0rem 1rem 1rem;
-    font-size: 2rem;
-    font-weight: 400;
-    &.with-bottom{
-        border-bottom: 1px solid rgba(240, 240, 240, 1);
-    }
-`
+	display: flex;
+	flex-direction: row;
+	width: 90%;
+	justify-content: space-between;
+	padding: 1rem 0rem 1rem 1rem;
+	font-size: 2rem;
+	font-weight: 400;
+	&.with-bottom {
+		border-bottom: 1px solid rgba(240, 240, 240, 1);
+	}
+`;
