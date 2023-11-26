8edbe7de40044daa7773ab9505d7a140c087453e
Gyeongbin Bak <tarabin49@gmail.com>
2023-11-26 18:19:29 +0900

    [FEAT]마이페이지 사진 수정 issue #47 배포 환경에서 테스트 필요할 것으로 보임.

--- src/components/mypage/EditProfile.tsx
+++ src/components/mypage/EditProfile.tsx
@@ -1,17 +1,106 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Header from '../common/Header'
 import styled from 'styled-components'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { userNameAtom, userPhotoAtom } from '@/app/recoilContextProvider'
-import Image from 'next/image'
 import { LuCamera } from "react-icons/lu";
+import { EditNickname, EditProfileImg } from '@/apis/mypage'
 
 const EditProfile = () => {
-    const userProfile = useRecoilValue(userPhotoAtom);
-    const userName = useRecoilValue(userNameAtom);
+    const [userProfile, setUserProfile] = useRecoilState(userPhotoAtom);
+    const [userName, setUserName] = useRecoilState(userNameAtom);
     const [changedName, setChangedName] = useState(userName);
     const [btnActivate, setBtnActivate] = useState('');
+    const [editDone, setEditDone] = useState(false);
+
+    const [image, setImage] = useState(userProfile);
+    const [imgIsChanges, setImgIsChanged] = useState(false);
+    const [imgData, setImgData] = useState<FormData>();
+
+    const fileInput = useRef<HTMLInputElement>(null)
+  
+    const handleImage = async (e: any) => {
+        // 내가 받을 파일은 하나기 때문에 index 0값의 이미지를 가짐
+        const file = e.target.files[0]
+        if(!file) return
+        
+        // 이미지 화면에 띄우기
+        const reader = new FileReader()
+        // 파일을 불러오는 메서드, 종료되는 시점에 readyState는 Done(2)이 되고 onLoad 시작
+        reader.readAsDataURL(file)
+        reader.onload = (e: any) => {
+            if(reader.readyState === 2) {
+                // 파일 onLoad가 성공하면 2, 진행 중은 1, 실패는 0 반환
+                setImage(e.target.result)
+            }
+        }
+        setBtnActivate('active');
+        setImgIsChanged(true);
+        const formData = new FormData();
+        formData.append('image', file);
+        setImgData(formData);
+    }
+
+    const handelChangeBtn = async() => {
+        if(userName === changedName){
+            //이미지만 바뀐 경우
+            const responseImg = await EditProfileImg(imgData!)
+            if(responseImg === false){
+                console.log('오류발생');
+                setBtnActivate('');
+            } else {
+                setUserProfile(responseImg.customProfile);
+                setEditDone(true);
+            }
+        } else {
+            if(imgIsChanges){
+                //둘 다 바뀐 경우
+                const responseName = await EditNickname(changedName);
+                if(responseName === 1002) {
+                    alert('닉네임 길이는 10자 이하만 가능합니다.');
+                    setBtnActivate('');
+                } else if(responseName === 1003) {
+                    alert('이미 사용중인 닉네임이 존재합니다.');
+                    setBtnActivate('');
+                } else if(responseName === false) {
+                    console.log('오류 발생');
+                    setBtnActivate('');
+                } else {
+                    //이미지 변환 수행
+                    const responseImg = await EditProfileImg(imgData!)
+                    if(responseImg === false){
+                        console.log('오류발생');
+                        setBtnActivate('');
+                    } else {
+                        setUserProfile(responseImg.customProfile);
+                        setEditDone(true);
+                    }
+                }
+            } else {
+                //이름만 바뀐 경우
+                const responseName = await EditNickname(changedName);
+                if(responseName === 1002) {
+                    alert('닉네임 길이는 10자 이하만 가능합니다.');
+                } else if(responseName === 1003) {
+                    alert('이미 사용중인 닉네임이 존재합니다.');
+                } else if(responseName === false) {
+                    console.log('오류 발생');
+                } else {
+                    setUserName(responseName.nickName);
+                    setEditDone(true);
+                }
+            }
+        }
+    }
+
+    useEffect(() => {
+        setChangedName(userName);
+        setBtnActivate('');
+        setImage(userProfile);
+        setImgIsChanged(false);
+    }, [editDone === true])
+
 
     useEffect(()=>{
         if(changedName === userName) {
@@ -28,27 +117,31 @@ const EditProfile = () => {
     <Header where="프로필 수정" />
     <ArticleWrapper>
         {
-            userProfile === '' ? 
-            <>
-            <ImgWrapper>
-                <img src="/img/profile-large.png" />
-                <EditBox className='use-image'>
+            // userProfile === '' ? 
+            // <>
+            // <ImgWrapper>
+            //     <img src="/img/profile-large.png" />
+            //     <EditBox className='use-image' htmlFor="input-file">
+            //         <LuCamera size="60%" />
+            //     </EditBox>
+            //     <input type="file" name="image_URL" id="input-file" accept='image/*'
+		    //     style={{ display : "none" }} ref={fileInput} onChange={(e) => handleImage(e)} />
+            // </ImgWrapper>
+            // </>
+            // :
+            <ImgBox $userprofile={image}>
+                <EditBox htmlFor="input-file">
                     <LuCamera size="60%" />
                 </EditBox>
-            </ImgWrapper>
-            </>
-            :
-            <ImgBox $userprofile={userProfile}>
-                <EditBox>
-                    <LuCamera />
-                </EditBox>
+                <input type="file" name="image_URL" id="input-file" accept='image/*'
+		        style={{ display : "none" }} ref={fileInput} onChange={(e) => handleImage(e)} />
             </ImgBox>
         }
         <UserNameWrapper>
             <div className='nickname'>닉네임</div>
             <UserNameInput value={changedName} onChange={(e) => setChangedName(e.target.value)} />
         </UserNameWrapper>
-        <CompletedBtn className={btnActivate}>수정 완료</CompletedBtn>
+        <CompletedBtn className={btnActivate} onClick={handelChangeBtn}>수정 완료</CompletedBtn>
     </ArticleWrapper>
     </>
   )
@@ -85,22 +178,31 @@ const ImgBox = styled.div<{$userprofile:string}>`
     display: flex;
     width: 15rem;
     height: 15rem;
-    border-radius: 20px;
-    overflow: hidden;
+    border-radius: 80px;
+    /* overflow: hidden; */
+    background-size: cover;
     background-image: ${props=>`url(${props.$userprofile})`};
     justify-content: flex-end;
     align-items: flex-end;
+    &:hover{
+        cursor: pointer;
+    }
+    & > input{
+        width: 100%;
+        height: 100%;
+    }
 `
 
-const EditBox = styled.div`
+const EditBox = styled.label`
     display: flex;
     width: 5rem;
     height: 5rem;
-    border-radius: 20px;
+    border-radius: 40px;
     justify-content: center;
     align-items: center;
     background-color: rgba(240, 240, 240, 1);
     overflow: hidden;
+    z-index: 1;
     &.use-image{
         position: absolute;
     }
