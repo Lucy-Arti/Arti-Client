8460e40aa5fb3919e8d302206d704bafb7c07146
Gyeongbin Bak <tarabin49@gmail.com>
2023-12-02 01:49:33 +0900

    [INIT]저장소 비우기

--- src/components/login/loginInfo.ts
+++
@@ -1,4 +0,0 @@
-const { VITE_API_KEY } = import.meta.env;
-
-export const Redirect_uri = 'https://arti-fashion.site/kakaologin';
-export const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_API_KEY}&redirect_uri=${Redirect_uri}&response_type=code`;
