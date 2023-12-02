5f7afc014d1d73ef7b337926b286b403469e68c5
gominzip <ahaminji@naver.com>
2023-12-02 22:41:28 +0900

    chore : 카카오로그인 리다이렉트 uri 변경

--- src/components/login/loginInfo.ts
+++ src/components/login/loginInfo.ts
@@ -1,7 +1,7 @@
-// export const Redirect_uri = 'https://arti-fashion.site/kakaologin';
+export const Redirect_uri = 'https://arti-fashion.site/kakaologin';
 
 export const KAKAO_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-export const Redirect_uri = 'http://localhost:3000/kakaologin';
+// export const Redirect_uri = 'http://localhost:3000/kakaologin';
 
 export const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${Redirect_uri}&response_type=code`;
