9ed7e7c8037559fc7ccd27d6af9a3bf13faa7a3d
Gyeongbin Bak <tarabin49@gmail.com>
2023-12-03 23:25:17 +0900

    [Fix]comment input에서 가로로 긴 사진 잘림 해결

--- src/components/productlist/CommentEdit.tsx
+++ src/components/productlist/CommentEdit.tsx
@@ -117,6 +117,7 @@ const CmtInputWrapper = styled.div`
         overflow: hidden;
         & > img{
             width: 100%;
+            height: 100%;
             object-fit: cover;
         }
     }
--- src/components/productlist/CommentInput.tsx
+++ src/components/productlist/CommentInput.tsx
@@ -130,6 +130,7 @@ const CmtInputWrapper = styled.div`
         overflow: hidden;
         & > img{
             width: 100%;
+            height: 100%;
             object-fit: cover;
         }
     }
