d4b0b5907b3a2235d39c85ddf62621adb48ab48d
CJY <cjy3458@naver.com>
2024-12-26 15:40:09 +0900

    fix: 투표 가능 여부 전역변수 항상 가능으로 수정정

--- src/app/recoilContextProvider.tsx
+++ src/app/recoilContextProvider.tsx
@@ -42,7 +42,7 @@ export const ListTabAtom = atom<string>({
 
 export const CanVoteAtom = atom<boolean>({
 	key: 'can-vote',
-	default: false,
+	default: true,
 	effects_UNSTABLE: [persistAtom],
 });
 
@@ -50,10 +50,10 @@ export const VoteEditAtom = atom({
 	key: 'vote-text',
 	default: {
 		bigger: '1차 콘테스트 투표가 \n종료되었습니다!',
-		smaller: '다음 콘테스트도 많은 참여 부탁드립니다.'
+		smaller: '다음 콘테스트도 많은 참여 부탁드립니다.',
 	},
 	effects_UNSTABLE: [persistAtom],
-})
+});
 
 export default function RecoidContextProvider({ children }: { children: React.ReactNode }) {
 	return <RecoilRoot>{children}</RecoilRoot>;
